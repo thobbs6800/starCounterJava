28d9fafbbecf27df31b225687ab5e8a75bfabded
Ty Hobbs <Ty@Tys-MacBook-Pro.local>
2017-02-13 15:38:30 -0600

    My First Commit

---
+++ src/PrintStarsHobbs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PrintStarsHobbs {
+	public static void main(String[] args)
+	
+	//Ty Hobbs
+	{
+		int counter = 1; 
+		
+		Scanner in =new Scanner (System.in);
+		//Ask the number of lines the user wants
+		System.out.println("How lines of printed stars would you like? ");
+		int numberOfLines =in.nextInt();
+		
+		do{
+			System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+			counter ++;
+		
+		}while(counter <=numberOfLines);
+		in.close();
+		
+	}
+}
